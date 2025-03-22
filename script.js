b0f39184aa9bdfed562b031cd92d5ce06b13ea3c
eshakar <102459513+eshakar@users.noreply.github.com>
2025-03-22 20:54:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 //your JS code here. If required.
+document.getElementById("btn").addEventListener("click", function() {
+    let age = document.getElementById("age").value;
+    let name = document.getElementById("name").value;
+
+    if (age === "" || name === "") {
+        alert("Please enter valid details.");
+        return;
+    }
+
+    let promise = new Promise(function(resolve, reject) {
+        setTimeout(() => {
+            if (age >= 18) {
+                resolve("Welcome, " + name + ". You can vote.");
+            } else {
+                reject("Oh sorry " + name + ". You aren't old enough.");
+            }
+        }, 2000);
+    });
+
+    promise.then(function(message) {
+        alert(message);
+    }
+
+    ).catch(function(message) {
+        alert(message);
+    });
+});
